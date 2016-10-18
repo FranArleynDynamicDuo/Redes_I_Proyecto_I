ff2dd9dedfa245c27f7ff7a31d9502bdd64f93b5
Arleyn Goncalves <arleyn.g.2712@gmail.com>
2016-10-17 21:05:04 -0430

    (AG) Prototipo del Cajero

---
+++ cajero.c
@@ -0,0 +1,128 @@
+/*
+ * Cajero.c
+ *
+ *  Created on: Oct 17, 2016
+ *      Author: Arleyn
+ */
+
+/* INCLUDES */
+#include <stdio.h>
+#include <time.h>
+
+
+/* DEFINES */
+
+
+/* ESTRUCTURES */
+
+/* FUNCTION SIGNATURES */
+void registrarDepositoEnBitacora();
+void registrarRetiroEnBitacora();
+void imprimeTicket();
+
+
+/* GLOBAL VARIABLES */
+int totalDisponible = 80000;
+char operation[1] = "i";
+int IdUsuario;
+char output[128];
+
+int main(int argc, char *argv[]) {
+
+	int i = 10;					// Iterador Multi-
+	int j = 0;
+
+	printf("Introduzca el Codigo del Usuario\n");
+	scanf("%d", &IdUsuario);
+
+	for (j; j<i ; j++) 
+		{
+
+			if (totalDisponible < 5000)
+				{
+					printf("AQUI SE MANDA UN MENSAJE AL SERVIDOR PARA RECARGAR\n");	
+					totalDisponible = 80000;
+				}
+
+			printf("Introduzca que accion desea realizar:\n");
+			scanf("%c", &operation[1]);
+
+			if (operation[1] == 'd')
+				{
+					int deposito;
+					printf("Introduzca el valor del deposito\n");
+				    scanf("%d", &deposito);
+			    	totalDisponible = totalDisponible + deposito;
+			    	printf("Valor del deposito: %d\n", deposito);
+			    	printf("Valor del Total Disponible: %d\n",totalDisponible);
+			    	imprimeTicket(&IdUsuario, &operation[1]);
+
+				}
+			else if (operation[1] == 'r')
+				{
+					int retiro;
+					printf("Introduzca el valor del retiro\n");
+				    scanf("%d", &retiro);
+
+						if 	(retiro <= totalDisponible)
+							{
+								if ( retiro <= 3000 )
+									{
+										totalDisponible = totalDisponible - retiro;
+						    			printf("Valor del retiro: %d\n", retiro);
+						    			printf("Valor del Total Disponible: %d\n",totalDisponible);
+						    			imprimeTicket(&IdUsuario, &operation[1]);
+					    			}
+					    		else
+					    			{
+					    				printf("El monto maximo de retiro es 3000\n");
+					    			}
+
+							}	
+						else
+							{
+								printf("Dinero No Disponible\n");
+							}
+				}
+
+		}
+
+}
+
+
+void registrarDepositoEnBitacora()
+{
+}
+
+void registrarRetiroEnBitacora()
+{
+}
+
+void imprimeTicket( int *idUsuario, char *opertion[1] ){
+	printf("\n");
+	printf("Se Imprime el Ticket del Usuario:\n");
+
+		time_t tiempo = time(0);
+	    struct tm *tlocal = localtime(&tiempo);
+	    
+	    strftime(output, 128, "%d/%m/%y", tlocal);
+		printf("Fecha: %s\n",output);
+		
+		strftime(output, 128, "%H:%M:%S", tlocal);
+		printf("Hora: %s\n",output);
+
+		printf("Codigo: %d\n", *idUsuario);
+
+	printf("\n");
+		
+		// FALTA IMPRIMIR EL TIPO DE OPERACION
+		// if ( *opertion[1] == 'd') 
+		// 	{
+		// 		printf("Operacion: Deposito");
+		// 	}
+		// else
+		// 	{
+		// 		printf("Operacion: Retiro");
+		// 	}
+		
+}
